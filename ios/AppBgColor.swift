ab9d71236dbd5ccce49388583c637f1e9035cd31
Aswin S <mr.aswin@gmail.com>
2021-12-01 19:43:36 +0530

    fix background color for native modal stack

--- ios/AppBgColor.swift
+++ ios/AppBgColor.swift
@@ -1,26 +1,15 @@
 @objc(AppBgColor)
 class AppBgColor: NSObject {
     
-    func hexStringToUIColor (hex:String) -> UIColor {
-        var cString:String = hex.trimmingCharacters(in: .whitespacesAndNewlines).uppercased()
-
-        if (cString.hasPrefix("#")) {
-            cString.remove(at: cString.startIndex)
+    @objc static func requiresMainQueueSetup() -> Bool {
+        return false
+    }
+    
+    @objc(setBackgroundColor:)
+    func setBackgroundColor(a: String) -> Void {
+        DispatchQueue.main.async {
+            self._setBgColor(color: a)
         }
-
-        if ((cString.count) != 6) {
-            return UIColor.white
-        }
-
-        var rgbValue:UInt64 = 0
-        Scanner(string: cString).scanHexInt64(&rgbValue)
-
-        return UIColor(
-            red: CGFloat((rgbValue & 0xFF0000) >> 16) / 255.0,
-            green: CGFloat((rgbValue & 0x00FF00) >> 8) / 255.0,
-            blue: CGFloat(rgbValue & 0x0000FF) / 255.0,
-            alpha: CGFloat(1.0)
-        )
     }
     
     func _setBgColor(color:String) -> Void {
@@ -29,12 +18,32 @@ class AppBgColor: NSObject {
         if let rootViewController = UIApplication.shared.windows.first!.rootViewController {
             rootViewController.view.backgroundColor = backgroundColor
         }
+        
+        if let window = UIApplication.shared.delegate?.window {
+            window?.backgroundColor = backgroundColor
+            window?.rootViewController?.view.backgroundColor = backgroundColor
+        }
     }
     
-    @objc(setBackgroundColor:)
-    func setBackgroundColor(a: String) -> Void {
-        DispatchQueue.main.async {
-            self._setBgColor(color: a)
+    func hexStringToUIColor (hex:String) -> UIColor {
+        var cString:String = hex.trimmingCharacters(in: .whitespacesAndNewlines).uppercased()
+        
+        if (cString.hasPrefix("#")) {
+            cString.remove(at: cString.startIndex)
         }
+        
+        if ((cString.count) != 6) {
+            return UIColor.white
+        }
+        
+        var rgbValue:UInt64 = 0
+        Scanner(string: cString).scanHexInt64(&rgbValue)
+        
+        return UIColor(
+            red: CGFloat((rgbValue & 0xFF0000) >> 16) / 255.0,
+            green: CGFloat((rgbValue & 0x00FF00) >> 8) / 255.0,
+            blue: CGFloat(rgbValue & 0x0000FF) / 255.0,
+            alpha: CGFloat(1.0)
+        )
     }
 }
